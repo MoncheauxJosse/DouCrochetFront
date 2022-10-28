748483e8d7a823b15ad2faddb1d4fc54cc85b101
Yacine Ouzgait <yacineouzgait59146@gmail.com>
2022-10-28 09:23:01 +0200

    [FEAT][YO] - "ListeProducts"

--- src/app/views/HomeView.jsx
+++ src/app/views/HomeView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import GenericCarousel from '../components/generics/GenericCaroussel';
 
 // import { ROLE_ADMIN } from '../constants/rolesConstant';
 // import { URL_ADMIN_HOME } from '../constants/urls/urlFrontEnd';
