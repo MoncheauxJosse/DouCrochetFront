d32fa21bcd66aae5df467b78e944d156dc7bff6d
nuages59 <nuages59@hotmail.fr>
2022-11-08 11:30:25 +0100

    resolve conflict

--- src/app/api/backend/users.js
+++ src/app/api/backend/users.js
@@ -1,13 +1,26 @@
-import {URL_BACK_DELETE, URL_GET_USERS} from '../../constants/urls/urlBackEnd';
+import {URL_BACK_DELETE, URL_BACK_MODIF, URL_GET_ROLES, URL_GET_USERS} from '../../constants/urls/urlBackEnd';
 import apiBackEnd from './api.Backend';
 
 export function getAll() {
     return apiBackEnd.get(URL_GET_USERS);
 }
-
+export function findRole() {
+    return apiBackEnd.get(URL_GET_ROLES);
+}
 export async function anonymizeUser(id) {
     try {
         const res = await apiBackEnd.put(URL_BACK_DELETE + id);
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function modifUser(id) {
+    try {
+        const res = await apiBackEnd.put(URL_BACK_MODIF + id);
+        console.log(res);
         console.log(`Status: ${res.status}`);
         console.log('Body: ', res.data);
     } catch (err) {
--- src/app/views/UsersView.jsx
+++ src/app/views/UsersView.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { anonymizeUser, getAll } from "../api/backend/users";
+import { anonymizeUser, findRole, getAll, modifUser } from "../api/backend/users";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const UsersView = () => {
   const [users, setUsers] = useState([]);
+  const [role, setRole] = useState([]);
   const [loader, setloader] = useState({ state: false });
+  
 
   useEffect(() => {
        getAll().then(userdata =>{
         setUsers(userdata.data);
       });
 
+    
+ 
+
     setloader({state:true}) 
   }, []);
 
+  useEffect(() => {
+    findRole().then(roleID =>{
+      console.log(roleID);
+    //  setUsers(roledata.data);
+   });
+  });
   if (loader.state == false) {
     return (<div>LOADING...</div>);
   } 
@@ -24,6 +35,11 @@ const UsersView = () => {
     anonymizeUser(id) 
     console.log("Utilisateur anonymisé")
   }
+  
+  const editUser = (id) => {
+    modifUser(id) 
+    console.log("Utilisateur modifier")
+  }
   const showToastMessage = () => {
     toast.success('Utilisateur anonymisé', {
         position: toast.POSITION.BOTTOM_LEFT
@@ -31,18 +47,18 @@ const UsersView = () => {
 };
 
   return (
-    <table class="min-w-full border text-center">
+    <table className="min-w-full border text-center">
       
       <thead className="bg-gray-50">
         <tr>
         
-        <th scope="col" class="text-sm font-medium text-black-900 px-6 py-4 border-r bg-blue-700">
+        <th scope="col" className="text-sm font-medium text-black-900 px-6 py-4 border-r ">
             Name
           </th>
-          <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 border-r">
             Email
           </th>
-          <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r bg-dark-pink">
+          <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 border-r ">
             Role
           </th>
         </tr>
@@ -54,6 +70,33 @@ const UsersView = () => {
             <td className="text-center border-b border-gray-200 ...">{user.email}</td>
             <td className="text-center border-b border-gray-200 ...">{user.role.role}</td>
             <td className="flex justify-center"><button onClick={() => {deleteUser(user._id); showToastMessage()}} className="delete-button border-b bg-red-500 rounded-md p-3">Supprimer</button></td>
+            <td className="flex justify-center">
+           
+
+              <tbody className="bg-white">
+            <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Modifier  Role</label>
+<select  id="role" class="bg-blue-50 border border-gray-300 text-pink-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-blue-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+   <option onClick={() => {editUser(role._id); showToastMessage()}} value="client">Client</option>
+   <option onSelect={() => {editUser(role._id); showToastMessage()}} value="commercial">Commercial</option>
+</select>
+  <button onClick={() => {editUser(user._id); showToastMessage()}} >confirmer</button>
+</tbody>
+             
+
+
+            {/* <select onClick={() => {editUser(user._id); showToastMessage()}} className="modif-button border-b bg-red-500 rounded-md p-3">Modifer
+            <option value=" client">client</option>
+            <option value=" commercial">commercial</option>
+        
+            </select> */}
+            </td>
+            <td>
+              
+
+
+            </td>
+
+            
           </tr>}
         </>
           ))}
@@ -61,6 +104,7 @@ const UsersView = () => {
     </table>
   );
 };
+
 }
 
 export default UsersView;
