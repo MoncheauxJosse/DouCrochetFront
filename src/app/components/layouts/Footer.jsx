a2c8b0388c693cdaf32dca0faac51202b10498b8
Elisa ROGGE <elisa.rogge59@gmail.com>
2022-11-30 10:30:22 +0100

    fix update user

--- src/app/components/layouts/Footer.jsx
+++ src/app/components/layouts/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
     const navigate= useNavigate()
 
     const click=(e)=>{
-        console.log("sa passe")
 
         navigate(URL.URL_SUPPORT);
 
@@ -18,15 +17,15 @@ const Footer = () => {
 
             <div className='flex justify-start mx-auto sm:px-6 ' > 
 
-                <div onClick={click} className=" py-2 px-6 text-white hover:text-light-yellow-hover underline ">
+                <div onClick={click} className="cursor-pointer py-2 px-6 text-white hover:text-light-yellow-hover underline ">
                     Contacter support. 
 
                 </div>
-                <div onClick="" className=" py-2 px-6 text-white hover:text-light-yellow-hover underline ">
+                <div className=" py-2 px-6 text-white hover:text-light-yellow-hover underline ">
                     FAQ
 
                 </div>
-                <div onClick="" className=" py-2 px-6 text-white hover:text-light-yellow-hover underline ">
+                <div className=" py-2 px-6 text-white hover:text-light-yellow-hover underline ">
                     Conditions générales de vente
 
 
